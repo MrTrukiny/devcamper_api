2f0b771054fe682fa81efc04b7b0dc8f653466bf
MrTrukiny <carloseduardo.tkn@gmail.com>
2019-10-28 00:25:07 -0500

    Use 'morgan' package as logging middleware

--- server2.js
+++ server2.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -16,13 +20,29 @@ const app = express();
 // });
 
 // You can send status directly
-app.get('/', (req, res) => {
-  // Way 1
-  // res.sendStatus(400);
-  // Way 2
-  // res.status(400).json({ sucess: false });
-  res.status(200).json({ success: true, data: { id: 1 } });
-});
+// app.get('/', (req, res) => {
+//   // Way 1
+//   // res.sendStatus(400);
+//   // Way 2
+//   // res.status(400).json({ sucess: false });
+//   res.status(200).json({ success: true, data: { id: 1 } });
+// });
+
+// Middleware example
+// const logger = (req, res, next) => {
+//   console.log(
+//     `Middleware ran: ${req.method} ${req.protocol}://${req.get('host')}${
+//       req.originalUrl
+//     }`
+//   );
+//   next();
+// };
+
+// Use logger middleware
+app.use(logger);
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
