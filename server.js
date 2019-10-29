94b075cbece97e6de36f14137704f8b4fbdc1364
MrTrukiny <carloseduardo.tkn@gmail.com>
2019-10-28 22:03:10 -0500

    Set MongoDB connection and create 'Bootcamp' mongoose model

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -22,6 +27,12 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`.yellow
+      .bold
   );
+});
+
+// Handle unhandle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
 });
